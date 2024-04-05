19a7e599ca7967cfbb5d6ba792900798b50096a3
Clément Nardi <clementnardi@gmail.com>
2024-04-05 22:20:57 +0200

    many improvements

--- encode_benchmark.sh
+++ encode_benchmark.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-input_video=./20240222_C0087.MP4
+video_path="/home/cnardi/Pictures/SONY ZV-E1_raw/20240223_C0092.MP4"
+video_folder=$(dirname "$video_path")
+video_name=$(basename "$video_path")
+
+script_dir=$(dirname "$0")
 
 videos_to_analyze=()
 
@@ -11,32 +15,32 @@ function parallel_vmaf {
 	echo "running VMAF analysis in parallel"
 	for video in "${videos_to_analyze[@]}" ; do
 		echo "running VMAF analysis for $video"
-		ffmpeg -i "$video" -i $input_video -lavfi libvmaf=log_path="${video}-vmaf.json":log_fmt=json -f null - </dev/null >/dev/null 2>/dev/null &
+		ffmpeg -i "$video" -i "$video_path" -lavfi libvmaf=log_path="${video}-vmaf.json":log_fmt=json -f null - </dev/null >/dev/null 2>/dev/null &
 	done
 	wait
 	videos_to_analyze=()
 }
 
-for i in $(seq 15 30) ; do
-	for p in ultrafast superfast veryfast faster fast medium slow slower veryslow ; do
-		video_file_name=${input_video}_${i}_${p}.mp4
+for i in $(seq 19 26) ; do
+	for p in ultrafast superfast veryfast faster fast medium slow slower ; do
+		video_file_path="$(realpath ${script_dir}/outputs/${video_name}_${i}_${p}.mp4)"
+
 		
-		if [ ! -f "$video_file_name" ] ; then
-			echo "missing $video_file_name -> encode"
+		if [ ! -f "$video_file_path" ] ; then
+			echo "missing $video_file_path -> encode"
 			begin=$(date +%s)
-			ffmpeg -i $input_video -c:v libx265 -preset $p -crf "$i" -c:a aac -b:a 256k "$video_file_name" >/dev/null 2>/dev/null
+			ffmpeg -i "$video_path" -c:v libx265 -preset $p -crf "$i" -c:a aac -b:a 256k "$video_file_path" >/dev/null 2>/dev/null
 			end=$(date +%s)
 			duration=$((end-begin))
-			echo $duration > "${video_file_name}.duration"
+			echo $duration > "${video_file_path}.duration"
+		fi
 
-			if [ ! -f "${video_file_name}-vmaf.json" ] ; then
-				videos_to_analyze+=("$video_file_name")
-			fi
-			# if more than 10 videos to analyze, run VMAF analysis in parallel
-			if [ ${#videos_to_analyze[@]} -ge 10 ] ; then
-				parallel_vmaf
-			fi
-
+		if [ ! -f "${video_file_path}-vmaf.json" ] ; then
+			videos_to_analyze+=("$video_file_path")
+		fi
+		# if more than 10 videos to analyze, run VMAF analysis in parallel
+		if [ ${#videos_to_analyze[@]} -ge 8 ] ; then
+			parallel_vmaf
 		fi
 	done
 done
--- get_stats.sh
+++ get_stats.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-input_video=./20240222_C0087.MP4
+script_dir=$(dirname "$0")
+video_path="/home/cnardi/Pictures/SONY ZV-E1_raw/20240223_C0092.MP4"
+video_folder=$(dirname "$video_path")
+video_name=$(basename "$video_path")
 
-osize=$(stat -c%s "$input_video")
+video_duration=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$video_path")
+
+osize=$(stat -c%s "$video_path")
 for p in ultrafast superfast veryfast faster fast medium slow slower veryslow ; do
 	echo -n ";$p;$p;$p"
 done
@@ -11,9 +16,10 @@ echo "CRF;VMAF;Size;Time;VMAF;Size;Time;VMAF;Size;Time;VMAF;Size;Time;VMAF;Size;
 for i in $(seq 15 30) ; do 
 	echo -n "$i;"
 	for p in ultrafast superfast veryfast faster fast medium slow slower veryslow ; do
-		video_file_name=${input_video}_${i}_${p}.mp4
-		vmaf_json=${video_file_name}-vmaf.json
-		duration=$video_file_name.duration
+
+		video_file_path="${script_dir}/outputs/${video_name}_${i}_${p}.mp4"
+		vmaf_json="${video_file_path}-vmaf.json"
+		duration="${video_file_path}.duration"
 		
 		if [ -f "$vmaf_json" ] ; then
 			echo -n "$(jq .pooled_metrics.vmaf.mean "$vmaf_json");"
@@ -21,15 +27,18 @@ for i in $(seq 15 30) ; do
 			echo -n ";"
 			
 		fi
-		if [ -f "$video_file_name" ] ; then
-			size=$(stat -c%s "$video_file_name")
+		if [ -f "$video_file_path" ] ; then
+			size=$(stat -c%s "$video_file_path")
 			ratio=$(bc -l <<< "$size / $osize")
 			echo -n "$ratio;"
 		else
 			echo -n ";"
 		fi
 		if [ -f "$duration" ] ; then
-			echo -n "$(cat "$duration");"
+			length="$(cat "$duration")"
+			operation="$length / $video_duration"
+			factor=$(bc -l <<< "$operation")
+			echo -n "$factor;"
 		else
 			echo -n ";"
 		fi
